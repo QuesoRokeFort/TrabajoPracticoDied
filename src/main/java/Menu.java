5ae3fdbea955f9f74e7c5f839d0141e221854603
PC <luciomuller81@gamil.com>
2023-11-09 19:26:22 -0300

    test

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-import java.sql.SQLException;
+
 import java.util.List;
 
 public class Menu {
@@ -10,27 +10,15 @@ public class Menu {
         //test
         int caso = 1;
         while (caso != 0) {
-            String tabla = new String();
+            String tabla ;
             caso = Gestor.swingestor.swingMenuListaBotones(6, List.of(new String[]{"Sucursal", "Producto","Stock","camino","grafo","pedidos"}));
             switch (caso) {
-                case 1:
-                    tabla= "sucursal";
-                    break;
-                case 2:
-                    tabla="producto";
-                    break;
-                case 3:
-                    tabla="stock";
-                    break;
-                case 4:
-                    tabla="camino";
-                    break;
-                case 5:
-                    Gestor.swingestor.menuGrafo();
-                    break;
-                case 6:
-                    tabla="ordenpedido";
-                    break;
+                case 1 -> tabla = "sucursal";
+                case 2 -> tabla = "producto";
+                case 3 -> tabla = "stock";
+                case 4 -> tabla = "camino";
+                case 5 -> Gestor.swingestor.menuGrafo();
+                case 6 -> tabla = "ordenpedido";
             }
             if (caso!=0 && caso !=5) Gestor.buscartabla(tabla);
         }
